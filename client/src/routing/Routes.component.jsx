83c52473b9358f1473c6e076015b6bf28c4c3838
Jeffrey <renjeffreyy@gmail.com>
2020-07-01 02:59:49 -0700

    tried to fix file name issue on heroku

--- client/src/routing/Routes.component.jsx
+++ client/src/routing/Routes.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 //components
-import Login from '../components/auth/Login.component';
+import Login from '../components/auth/login.component';
 import Register from '../components/auth/Register.component';
 import Appointments from '../components/layout/Appointments';
 import Alert from '../components/layout/Alert';
