30cc069c4ed58d6553dee398e23e5312310a2d54
Jeffrey <renjeffreyy@gmail.com>
2020-07-01 04:36:09 -0700

    testing routes issue on heroku, added path requirement to index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-
+const path = require('path');
 const express = require('express');
 const connectDB = require('./db');
 
@@ -21,7 +21,7 @@ app.use('/api/calendar', require('./routes/api/googleCalendar'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
